49f388c69c8034a1fd78196c689e9f2cf9346f37
barakov14 <playmagma4@gmail.com>
2024-08-05 18:55:43 +0500

    app init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyBunD-mVDDO4JTWb2GJLsGhWmP3DL0t6AQ",
+      authDomain: "flashcardmake.firebaseapp.com",
+      projectId: "flashcardmake",
+      storageBucket: "flashcardmake.appspot.com",
+      messagingSenderId: "150294929447",
+      appId: "1:150294929447:web:0b553da494025c9393ae02",
+      measurementId: "G-32TBQC8RX8"
+    })),
+    provideHttpClient(),
+    provideFirestore(() => getFirestore())
+  ]
 };
