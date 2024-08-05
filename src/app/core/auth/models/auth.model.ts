c18521ee2b8c9d0ce5b3b990fd6d540b71b3c219
barakov14 <playmagma4@gmail.com>
2024-08-05 19:19:28 +0500

    created auth service

---
+++ src/app/core/auth/models/auth.model.ts
@@ -0,0 +1,14 @@
+export interface ISignInRequest {
+  email: string;
+  password: string;
+}
+
+export interface ISignIn {
+  email: string;
+  password: string;
+}
+
+export interface ISignUpRequest {
+  email: string;
+  password: string;
+}
