16517e879fd0769a5d1cd4ac13c87dc37d3da810
barakov14 <playmagma4@gmail.com>
2024-08-08 16:45:10 +0500

    environment configure

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBunD-mVDDO4JTWb2GJLsGhWmP3DL0t6AQ',
+    authDomain: 'flashcardmake.firebaseapp.com',
+    projectId: 'flashcardmake',
+    storageBucket: 'flashcardmake.appspot.com',
+    messagingSenderId: '150294929447',
+    appId: '1:150294929447:web:0b553da494025c9393ae02',
+    measurementId: 'G-32TBQC8RX8',
+  },
+};
